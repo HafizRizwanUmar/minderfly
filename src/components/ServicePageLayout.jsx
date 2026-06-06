7a2825a08725c17db56d7096da7bd5774bea7d6f
Hafiz Rizwan Umar <teckleoutreach@gmail.com>
2026-06-06 06:55:02 +0500

    Fix SEO and routing issues: allow assets in robots.txt, fix Nginx trailing slash redirect, add missing Link import to ServicePageLayout

--- src/components/ServicePageLayout.jsx
+++ src/components/ServicePageLayout.jsx
@@ -1,5 +1,6 @@
 import React, { useRef, useState, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
+import { Link } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import SEOHead from './SEOHead';
